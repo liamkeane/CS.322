436a3ce72c16cf7afdb18c092293ac6ea8b48279
Liam Keane <keanel@carleton.edu>
2024-10-21 16:01:06 -0500

    Wrote metric functions

--- Text Classification/classify.py
+++ Text Classification/classify.py
@@ -9,6 +9,8 @@ import numpy as np
 import tqdm
 import sklearn.metrics
 import collections
+from collections import defaultdict
+import math
 
 
 global _TOKENIZER
@@ -97,34 +99,36 @@ def get_nb_probs(train_docs,
     pos_word_count = 0
     neg_word_count = 0
     total_word_count = 0
-    pos_probs = {}
-    neg_probs = {}
+    pos_probs = defaultdict(lambda: smooth)
+    neg_probs = defaultdict(lambda: smooth)
 
     for i in range(len(train_docs)):
-        if train_labels[i] == 0:
+        if train_labels[i] == 1:
             for word in train_docs[i]:
                 if word not in pos_probs and word not in neg_probs:
                     total_word_count += 1
-                    
+
                 pos_word_count += 1
-                pos_probs.update({word: (pos_probs.setdefault(word, smooth) + 1)})
+                pos_probs.update({word: (pos_probs[word] + 1)})
             n_pos += 1
         else:
             for word in train_docs[i]:
                 if word not in pos_probs and word not in neg_probs:
                     total_word_count += 1
-                    
+
                 neg_word_count += 1
-                neg_probs.update({word: (neg_probs.setdefault(word, smooth) + 1)})
+                neg_probs.update({word: (neg_probs[word] + 1)})
             n_neg += 1
 
-
-    # TODO: add the smoothing to the denominators
-    for (word, count) in pos_probs.items():
-        pos_probs.update({word: count/pos_word_count})
+    for word, count in pos_probs.items():
+        pos_probs.update({word: (count + smooth)/(pos_word_count + (smooth * total_word_count))})
     
-    for (word, count) in neg_probs.items():
-        neg_probs.update({word: count/neg_word_count})
+    for word, count in neg_probs.items():
+        neg_probs.update({word: (count + smooth)/(neg_word_count + (smooth * total_word_count))})
+
+    # define values for unknown tokens
+    pos_probs.default_factory = lambda:(smooth/(pos_word_count + (smooth * total_word_count)))
+    neg_probs.default_factory = lambda:(smooth/(neg_word_count + (smooth * total_word_count)))
 
     return pos_probs, neg_probs, n_pos, n_neg
     
@@ -135,28 +139,72 @@ def classify_doc_with_naive_bayes(doc_in,
                              n_pos,
                              n_neg):
     '''Given an input document and the outputs of get_nb_probs, this
-    function comptues the summed log probability of each class given
+    function computes the summed log probability of each class given
     the input document, according to naive bayes. If the token-summed
     positive log probability is greater than the token-summed negative
     log probability, then this function outputs 1. Else, it outputs 0.
     '''
-    raise NotImplementedError('TODO')
+    
+    pos_doc_prob = math.log(n_pos/(n_pos + n_neg))
+    neg_doc_prob = math.log(n_neg/(n_pos + n_neg))
 
+    for word in doc_in:
+        if word in pos_probs or word in neg_probs:
+            pos_doc_prob += math.log(pos_probs[word])
+            neg_doc_prob += math.log(neg_probs[word])
+    
+    if pos_doc_prob > neg_doc_prob:
+        return 1
+    else:
+        return 0
+
+### Page 67 of textbook describes the following metrics ### 
 
 def get_accuracy(true, predicted):
-    raise NotImplementedError('TODO')
-
+    tpv = np.vectorize(true_positive)
+    tnv = np.vectorize(true_negative)
+    tpv_a = tpv(true, predicted)
+    tnv_a = tnv(true, predicted)
+    accuracy = (sum(tpv_a)+sum(tnv_a))/tpv_a.size
+    return accuracy
 
 def get_precision(true, predicted):
-    raise NotImplementedError('TODO')
-
+    tpv = np.vectorize(true_positive)
+    fpv = np.vectorize(false_positive)
+    tpv_a = tpv(true, predicted)
+    fpv_a = fpv(true, predicted)
+    precision = sum(tpv_a)/(sum(tpv_a)+sum(fpv_a))
+    return precision
 
 def get_recall(true, predicted):
-    raise NotImplementedError('TODO')
-
+    tpv = np.vectorize(true_positive)
+    fnv = np.vectorize(false_negative)
+    tpv_a = tpv(true, predicted)
+    fnv_a = fnv(true, predicted)
+    recall = sum(tpv_a)/(sum(tpv_a)+sum(fnv_a))
+    return recall
 
 def get_f1(true, predicted):
-    raise NotImplementedError('TODO')
+    precision = get_precision(true, predicted)
+    recall = get_recall(true, predicted)
+    f1 = (2*precision*recall)/(precision+recall)
+    return f1
+
+
+def true_positive(true, predicted):
+    return int(true == 1 and predicted == 1)
+
+
+def false_positive(true, predicted):
+    return int(true == 0 and predicted == 1)
+
+
+def false_negative(true, predicted):
+    return int(true == 1 and predicted == 0)
+
+
+def true_negative(true, predicted):
+    return int(true == 0 and predicted == 0)
 
 
 def get_metrics(true, predicted):
@@ -191,20 +239,26 @@ def main():
     #                                                                       ('dissapointing', -1)])
     #                              for v in val_docs])
 
-    # ## Naive bayes
+    # Naive bayes
     pos_probs, neg_probs, n_pos, n_neg = get_nb_probs(train_docs, train_labels)
     # print("Probability of a word appearing given positive:\n", pos_probs)
-    print("Probability of a word appearing given negative:\n", neg_probs)
+    # print("Probability of a word appearing given negative:\n", neg_probs)
     # print("Number of positive documents:", n_pos)
-    print("Number of negative documents:", n_neg)
-    # nb_predictions = np.array([classify_doc_with_naive_bayes(d, pos_probs, neg_probs, n_pos, n_neg)
-    #                            for d in val_docs])
+    # print("Number of negative documents:", n_neg)
+    nb_predictions = np.array([classify_doc_with_naive_bayes(d, pos_probs, neg_probs, n_pos, n_neg)
+                               for d in val_docs])
+    # print(nb_predictions)
+    # i = 0
+    # for prediction in nb_predictions:
+    #     print("Predicted:", prediction, ": Labeled", val_labels[i])
+    #     i += 1
+    
 
-    # # NLP folks sometimes multiply metrics by 100 simply for aesthetic reasons
+    # NLP folks sometimes multiply metrics by 100 simply for aesthetic reasons
     # print(' & '.join(['{:.2f}'.format(100*f)
     #                   for f in get_metrics(val_labels, hand_predictions)]))
-    # print(' & '.join(['{:.2f}'.format(100*f)
-    #                   for f in get_metrics(val_labels, nb_predictions)]))
+    print(' & '.join(['{:.2f}'.format(100*f)
+                      for f in get_metrics(val_labels, nb_predictions)]))
     
     
 if __name__ == '__main__':
