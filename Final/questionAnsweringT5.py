e58f3086d978b9642cbf87b015093707d491140f
Liam Keane <keanel@carleton.edu>
2024-11-22 14:23:40 -0600

    wrote predictionary builder
    Co-authored-by: Lazuli <Clean-Hands@users.noreply.github.com>

--- Final/questionAnsweringT5.py
+++ Final/questionAnsweringT5.py
@@ -1,17 +1,64 @@
 
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+import json
 
-tokenizer = AutoTokenizer.from_pretrained("deepset/flan-t5-xl-squad2")
-model = AutoModelForQuestionAnswering.from_pretrained("deepset/flan-t5-xl-squad2")
 
-question, text = "Which company produces the iPod?", "The iPod is a line of portable media players and multi-purpose pocket computers designed and marketed by Apple Inc. The first line was released on October 23, 2001, about 8½ months after iTunes (Macintosh version) was released. The most recent iPod redesigns were announced on July 15, 2015. There are three current versions of the iPod: the ultra-compact iPod Shuffle, the compact iPod Nano and the touchscreen iPod Touch."
+def get_response(question, document):
+    global model, tokenizer
+    inputs = tokenizer(question, document, return_tensors="pt")
+    outputs = model(**inputs)
 
-inputs = tokenizer(question, text, return_tensors="pt")
-outputs = model(**inputs)
+    answer_start_index = outputs.start_logits.argmax()
+    answer_end_index = outputs.end_logits.argmax()
 
-answer_start_index = outputs.start_logits.argmax()
-answer_end_index = outputs.end_logits.argmax()
+    predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
+    return tokenizer.decode(predict_answer_tokens)
 
-predict_answer_tokens = inputs.input_ids[0, answer_start_index : answer_end_index + 1]
-print(tokenizer.decode(predict_answer_tokens))
+
+'''
+initialize prediction dictionary.
+for each topic:
+    for each document in the topic:
+        for each question in the document:
+            prompt the model with the question and document.
+            add dictionary item with question id and the model's reponse
+'''
+def build_and_write_predictionary():
+    with open("dev-v2.0.json") as f:
+        dev = json.load(f)
+
+    dev_set = dev["data"]
+    
+    predictionary = {}
+
+    i = 1
+    for topic in dev_set:
+        
+        print("Topic", str(i) + "/" + str(len(dev_set) + 1))
+        topic_paras = topic["paragraphs"]
+        
+        for paragraph in topic_paras:
+            document = paragraph["context"]
+
+            for question in paragraph["qas"]:
+                q_text = question["question"]
+                q_id = question["id"]
+                response = get_response(q_text, document)
+                predictionary.update({q_id: response})
+                
+        i += 1
+                
+    # write predictionary to output file
+    with open('out.json', 'w') as out_file: 
+        out_file.write(json.dumps(predictionary))
+                
+
+def main():
+    build_and_write_predictionary()
+    
+    
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained("deepset/flan-t5-xl-squad2")
+    model = AutoModelForQuestionAnswering.from_pretrained("deepset/flan-t5-xl-squad2")
+    main()
